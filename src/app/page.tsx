b75ae79572571e8a18c28f9dd18d2cfd4310156d
donniean <acmilank22@gmail.com>
2023-07-21 23:39:43 +0800

    chore: css modules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,12 @@ import Image from 'next/image';
 export default function Root() {
   return (
     <div className="flex flex-col items-center justify-center p-[32px]">
-      <Image alt="Next.js Logo" src="/assets/images/logo.svg" />
+      <Image
+        alt="Next.js Logo"
+        height={512}
+        src="/assets/images/logo.svg"
+        width={512}
+      />
       <h1 className="pt-8 text-5xl">Hello Next.js</h1>
     </div>
   );
