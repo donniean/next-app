fbc172d4216f5970fafad773b2ba328efd1309af
donniean <acmilank22@gmail.com>
2023-07-21 22:46:25 +0800

    chore: add d.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Logo from '@/assets/images/logo.svg';
+import Image from 'next/image';
 
 export default function Root() {
   return (
     <div className="flex flex-col items-center justify-center p-[32px]">
-      <Logo />
+      <Image alt="Next.js Logo" src="/assets/images/logo.svg" />
       <h1 className="pt-8 text-5xl">Hello Next.js</h1>
     </div>
   );
