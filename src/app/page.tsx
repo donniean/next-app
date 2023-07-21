a937b182767bc4e2915ee446e3e96cbb214e8d48
donniean <acmilank22@gmail.com>
2023-07-22 00:42:58 +0800

    chore: update ESLint rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Root() {
   return (
     <div className="flex flex-col items-center justify-center p-[32px]">
       <Image
-        className="animate-bounce"
+        className="animate-pulse"
         src="/assets/images/logo.svg"
         height={512}
         width={512}
