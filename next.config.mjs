daaed52a28d18bba6d9758bddb2f5acd52ec5dbf
donniean <acmilank22@gmail.com>
2024-04-18 13:34:30 +0800

    chore: update configs (#499)
    * chore: update configs
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    * chore: update configs
    
    Signed-off-by: donniean <acmilank22@gmail.com>
    
    ---------
    
    Signed-off-by: donniean <acmilank22@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: config => {
+  webpack: (config) => {
     const rules = config.module.rules
-      .find(rule => typeof rule.oneOf === 'object')
-      .oneOf.filter(rule => Array.isArray(rule.use));
-    rules.forEach(rule => {
-      rule.use.forEach(moduleLoader => {
+      .find((rule) => typeof rule.oneOf === 'object')
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+    rules.forEach((rule) => {
+      rule.use.forEach((moduleLoader) => {
         if (
           moduleLoader.loader !== undefined &&
           moduleLoader.loader.includes('css-loader') &&
